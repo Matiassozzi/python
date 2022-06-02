3b1fe13db9e47c594f34d8b4385d1de8703929b7
Matiassozzi <106718009+Matiassozzi@users.noreply.github.com>
2022-06-01 23:18:33 -0300

    Add files via upload

---
+++ calculadora de m2.py
@@ -0,0 +1,5 @@
+n1 = float(input("Introduce el alto: ") )
+n2 = float(input("Introduce el ancho: ") )
+suma=n1*n2
+print(suma)
+print("metros cuadrados")
---
+++ promedio alumno.py
@@ -0,0 +1,9 @@
+
+nota1= float(input("introduce la nota del primer trimestre:")) 
+nota2= float(input("introduce la nota del segundo trimestre:")) 
+nota3= float(input("introduce la nota del tercer trimestre:")) 
+promedio=(nota1+nota2+nota3)/3
+if promedio > 7:
+    print("aprobado")
+else :
+    print("Desaprobado")
